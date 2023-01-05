b522f345e6de6597ded8e16454ab5976d3e2e833
bmaxdk <choh8uw@gmail.com>
2023-01-04 22:04:59 -0500

    Create Python Service Server and Client

---
+++ Master_the_Key_ROS_Concepts/Lec1_Create_a_Cakin_Workspace/catkin_ws/src/my_robot_tutorials/scripts/add_two_ints_client.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env Python3
+
+import rospy
+from rospy_tutorials.srv import AddTwoInts
+
+
+if __name__ == '__main__':
+    rospy.init_node("add_two_ints_client")
+
+    # Need to create service client
+    rospy.wait_for_service("/add_two_ints") # /name of the service   where it advertise
+
+    # Create the client to call the service
+    # If the client fail the client server, it will need exception
+    try:
+        # Call the service
+        # Create client
+        add_two_ints = rospy.ServiceProxy("/add_two_ints", AddTwoInts)
+        response = add_two_ints(2,6) #call the client (2,6) is where a and b
+        rospy.loginfo("Sum is : " +str(response.sum))
+    except rospy.ServiceExcetion as e:
+        rospy.lowarn("Service Failed: "+ str(e))
+
---
+++ Master_the_Key_ROS_Concepts/Lec1_Create_a_Cakin_Workspace/catkin_ws/src/my_robot_tutorials/scripts/add_two_ints_server.py
@@ -0,0 +1,21 @@
+#!usr/bin/env python3
+
+import rospy
+from rospy_tutorials.srv import AddTwoInts
+# message tpye will use rospy_turoials/AddTwoInts.srv
+
+def handle_add_two_ints(req): #req is from AddTwoInts
+    result = req.a + req.b
+    rospy.loginfo("Sum of " + str(req.a) + " and " + str(req.b) + " is " + str(result))
+    return result
+
+if __name__ == '__main__':
+    rospy.init_node("add_two_ints_server")
+    rospy.loginfo("Add two ints server node created")
+
+    # Create service server
+    service = rospy.Service("/add_two_ints",AddTwoInts, handle_add_two_ints) #we need function callback which service surver requests
+
+    rospy.loginfo("Service server has been started")
+
+    rospy.spin()

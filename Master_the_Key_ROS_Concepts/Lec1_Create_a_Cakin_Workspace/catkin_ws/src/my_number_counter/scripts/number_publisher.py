558c581819bc746a4628006388245b6687434c07
bmaxdk <choh8uw@gmail.com>
2023-01-14 19:09:21 -0500

    Add rosparam python

--- Master_the_Key_ROS_Concepts/Lec1_Create_a_Cakin_Workspace/catkin_ws/src/my_number_counter/scripts/number_publisher.py
+++ Master_the_Key_ROS_Concepts/Lec1_Create_a_Cakin_Workspace/catkin_ws/src/my_number_counter/scripts/number_publisher.py
@@ -7,14 +7,26 @@ if __name__ == '__main__':
     rospy.init_node('number_publisher', anonymous=True)
 
     pub = rospy.Publisher("/number", Int64, queue_size=10)
-
-    rate = rospy.Rate(2)
     
+    # [rosparam] ex:publish_frequency = rospy.get_param("/nameofparam")
+    publish_frequency = rospy.get_param("/number_publish_frequency")
+    rate = rospy.Rate(publish_frequency)
+
+    
+    #rate = rospy.Rate(2)
+    
+    # [rosparam] for msg.data
+    number = rospy.get_param("/number_to_publish")
+
+    # [rosparam] We can also set param here
+    rospy.set_param("/another_param", "Hello")
+
     msg = Int64()
     msg.data = -1
     while not rospy.is_shutdown():
         pub.publish(msg)
-        msg.data += 1
+        #msg.data += 1
+        msg.data = number
         rate.sleep()
         
     rospy.loginfo("Node was stopped")
